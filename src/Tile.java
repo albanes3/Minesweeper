7adb20724e607f14d717ff5261e078e8f715e50f
TankMissile <TankMissile@users.noreply.github.com>
2014-02-24 20:20:18 -0600

    Update Tile.java
    Added setLoc(int x, int y); which stores the location on the board (so that it may be passed to the Grid to reveal surrounding tiles, if possible/needed, or to anything else for that matter)
    
    removed explode();, as it can be achieved at a higher level by revealing tiles known to be mines and separately setting the game over flag.

--- src/Tile.java
+++ src/Tile.java
@@ -4,16 +4,21 @@ public class Tile {
 	public static int[] red = {0,0};
 	public static int[] white = {3,0};
 	public static int size = 20;
-	private static int rank = 0; //how many mines are adjacent?
-	public static boolean mine = 0; //is this a mine?
-	public static int status = 0; //represents visual state to user - blank (0), marked (1), ? (2), or revealed (3)
 	public static Image image;
 	
 	private static int x_loc; //x location on the board
 	private static int y_loc; //y location on the board
+	private static int rank = 0; //how many mines are adjacent?
+	public static boolean mine = 0; //is this a mine?
+	public static int status = 0; //represents visual state to user - blank (0), marked (1), ? (2), or revealed (3)
 	
 	public Tile(){
 		image = RL.getImage("tile.jpg");
+	}
+	
+	public setLoc(int x, int y){
+		this.x_loc = x;
+		this.y_loc = y;
 	}
 	
 	//Increase the tile's rank when a mine is placed
@@ -31,13 +36,8 @@ public class Tile {
 	//reveal the tile
 	public void reveal(){
 		status = 3; //set revealed status
-		if(mine) this.explode(); //boom.
-		else if(rank == 0){ //if 0 tile, reveal surrounding tiles
+		if(rank == 0){ //if 0 tile, reveal surrounding tiles
 			//TODO: get surrounding tiles from grid and call this method on them
 		}
-	}
-	
-	private explode(){
-		//TODO: Tell Game object the game has ended.
 	}
 }
