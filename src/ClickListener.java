378e929dd54a68e259ecefcbc57428adbb2b790a
TankMissile <TankMissile@users.noreply.github.com>
2014-02-26 01:32:14 -0600

    Update ClickListener.java

--- src/ClickListener.java
+++ src/ClickListener.java
@@ -3,45 +3,60 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 public class ClickListener implements MouseListener,MouseMotionListener{
+	public static  boolean mouseRight = false;
+	private static  boolean mouseLeft = false;
 
-	
+	public static boolean MouseLeft(){
+		return mouseLeft;
+	}
+
+	public static boolean MouseRight(){
+		return mouseRight;
+	}
+
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 	public void mousePressed(MouseEvent e) {
 		if(e.getButton() == MouseEvent.BUTTON1){
-			Game.mouseLeft = true;
+			mouseLeft = true;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON2){
+			mouseRight = true;
 		}
 	}
 
-	
+
 	public void mouseReleased(MouseEvent e) {
 		if(e.getButton() == MouseEvent.BUTTON1){
-			Game.mouseLeft = false;
+			mouseLeft = false;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON2){
+			mouseRight = false;
 		}
 	}
 
-	
+
 	public void mouseDragged(MouseEvent e) {
 		Game.point.setLocation(e.getX(),e.getY());
 	}
 
-	
+
 	public void mouseMoved(MouseEvent e) {
 		Game.point.setLocation(e.getX(),e.getY());
 	}
