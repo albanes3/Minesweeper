7eebc4d311d101b60e054b529854310bc5443684
albanes3 <albanes3@uic.edu>
2014-02-28 16:37:45 -0600

    Update GameMenu.java

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,25 +1,30 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.*;
+import java.io.IOException;
 
 import javax.swing.*;
 
 public class GameMenu extends JMenuBar implements ActionListener{
-	private JFrame j = new JFrame();
+	private static final long serialVersionUID = 1L;
+	JButton resetButt;
 	public GameMenu(){
 		//JMenuBar menuBar = new JMenuBar(); //create the panel for the menu
 		JMenu gameMenu = new JMenu("Game"), helpMenu = new JMenu("Help"); //create the two menus
 
 		//create menu items for both menus
-		JMenuItem resetMenuButton = new JMenuItem("Reset", KeyEvent.VK_R), 
+		JMenuItem resetMenuButton = new JMenuItem("Reset game", KeyEvent.VK_R), 
 				topTenButton = new JMenuItem("Top Ten", KeyEvent.VK_T),
 				exitButton = new JMenuItem("eXit", KeyEvent.VK_X),
 				helpButton = new JMenuItem("Help", KeyEvent.VK_H),
-				aboutButton = new JMenuItem("About", KeyEvent.VK_A);
+				aboutButton = new JMenuItem("About", KeyEvent.VK_A),
+				resetScoresButton = new JMenuItem("Reset Scores", KeyEvent.VK_S);
 
 		//Details for "Game" menu
 		gameMenu.setMnemonic(KeyEvent.VK_G);
+		
+		resetScoresButton.addActionListener(this);
+		resetScoresButton.setActionCommand("Reset Scores");
 
 		topTenButton.addActionListener(this);
 		topTenButton.setActionCommand("Top Ten");
@@ -32,9 +37,10 @@ public class GameMenu extends JMenuBar implements ActionListener{
 
 		gameMenu.add(resetMenuButton);
 		gameMenu.add(topTenButton);
+		gameMenu.add(resetScoresButton);
 		gameMenu.add(exitButton);
-
-
+	
+		
 		//Details for "Help" menu
 		helpMenu.setMnemonic(KeyEvent.VK_H);
 
@@ -50,6 +56,11 @@ public class GameMenu extends JMenuBar implements ActionListener{
 		//add menus to menu bar
 		this.add(gameMenu);
 		this.add(helpMenu);
+		
+		resetButt = new JButton("Reset");
+		resetButt.setBounds(207,170,70,20);
+		resetButt.addActionListener(this);
+		resetButt.setActionCommand("Reset");
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -58,20 +69,38 @@ public class GameMenu extends JMenuBar implements ActionListener{
 		}
 		else if("help".equals(e.getActionCommand())){
 			//j.setVisible(true);
-			JOptionPane.showMessageDialog(null,"Rules:\n\n- Left click on a tile to reveal the square.\n- Numbers on tiles represent the number of adjacent bombs.\n- Right click on a tile to mark it as a bomb.\n- Right click again to mark it as a possible bomb.\n- Right click a third time to clear markings.\n- The game is over once you click on a bomb.\n- To win, reveal all tiles that are not bombs.\n- There is a counter at the bottom representing the number of bombs left.\n ");
+			JOptionPane.showMessageDialog(null,"Rules:\n\n- Left click on a tile to reveal the square.\n- Numbers on tiles represent the number of adjacent bombs.\n- Right click on a tile to mark it as a bomb.\n- Right click again to mark it as a possible bomb.\n- Right click a third time to clear markings.\n- The game is over once you click on a bomb.\n- To win, reveal all tiles that are not bombs.\n- There is a counter at the bottom representing the number of bombs left.\n- In order to be on the high score list, you must have one of the top ten fastest times!\n\n");
 		}
 		else if("About".equals(e.getActionCommand())){
-			JOptionPane.showMessageDialog(null,"By: Mike Albanese and Trevor Evans\n\n                    CS342\n" );
+			JOptionPane.showMessageDialog(null,"By: Mike Albanese; Trevor Evans\n\n                  CS342\n" );
 		}
 		else if("Reset".equals(e.getActionCommand())){
 			Game.resetGame();
 		}
+		else if("Reset Scores".equals(e.getActionCommand())){
+			Level.topScores = new String("           --Top Scores--\n\n");
+			for(int i = 0; i < 10; i++){
+				Level.scores[i] = 0;
+				Level.names[i] = null;
+			}
+			try{
+				Game.file.delete();
+			}catch(Exception io){
+				io.printStackTrace();
+			}
+			
+			try{
+				if (!Game.file.exists()) {
+					Game.file.createNewFile();
+				}
+			}catch(IOException ioe){
+				ioe.printStackTrace();
+			}
+		}
 		else if("Top Ten".equals(e.getActionCommand())){
-			String toptenString = "Top Ten";
-			JOptionPane.showMessageDialog(null, toptenString);
+			JOptionPane.showMessageDialog(null, Level.topScores);
 		}
 
 	}
 }
-
 
