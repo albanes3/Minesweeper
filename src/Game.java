91d9329e386399e45560a92855a190dc4adba48d
TankMissile <TankMissile@users.noreply.github.com>
2014-02-28 19:51:03 -0600

    Comments
    Added comments and reorganized variables to be more readable

--- src/Game.java
+++ src/Game.java
@@ -1,34 +1,39 @@
 import java.awt.*;
 import java.io.*;
-
 import javax.swing.*;
 
 public class Game extends Canvas implements Runnable{
-	public static boolean gtfo=false;
-	public static boolean hitBomb=false;
 	private static final long serialVersionUID = 1L;
-	public static int width = 290;
-	public static int height = width-35;
+	
 	public static Dimension size = new Dimension(width,height);
+	
 	private Image image;
-	public static boolean gamewon=false;
+	
 	public static File file = new File("topten.txt");
-	public static Level level;
-	public static JFrame j;
-	public static JTextArea bombCount, timeCount;
-	public static long startTime,currentTime,stopTime;
 	private static FileWriter fw;
+	private static BufferedWriter bw;
+	
+	public static Level level; //Runs the 10x10 grid of tiles
+	public static JFrame j;
+	public static JTextArea bombCount, timeCount; //Fields to display elapsed time and bombs remaining
+	public static Point point = new Point(0,0); //Checks against tile location to determine clicks
+	
+	public static long startTime,currentTime,stopTime;
+	public static int width = 290; //frame width
+	public static int height = width-35; //frame height
 	public static int gameStart=0;
 
-	public static Point point = new Point(0,0);
-	private static BufferedWriter bw;
-	public static String[] toptens = new String[10];
+	public static boolean running = false; //stores whether the game is running or not
+	public static boolean windowrunning = false; //stores whether the entire application is running or should close
+	public static boolean gtfo=false; //tells the application to close the window and stop running
+	public static boolean hitBomb=false;
+	public static boolean gamewon=false;
+	
+	public static String[] toptens = new String[10]; //array storing top ten high scores
 
-	public static boolean running = false;
-	public static boolean windowrunning = false;
-
+	//Constructor
 	public Game(){
-		//create file
+		//create file for top tens
 		try{
 			if (!file.exists()) {
 				file.createNewFile();
@@ -36,44 +41,17 @@ public class Game extends Canvas implements Runnable{
 		}catch(IOException e){
 			e.printStackTrace();
 		}
+		
+		
 		for(int i=0; i<toptens.length;i++)
-			toptens[i]="content";
+			toptens[i]=""; //initialize the top tens array
 
 
 		addMouseListener(new ClickListener());
 		addMouseMotionListener(new ClickListener());
 	}
-	public static boolean MouseLeft(){//checks for left click
-		return ClickListener.MouseLeft();
-	}
-
-	public static boolean MouseRight(){//checks for right click
-		return ClickListener.MouseRight();
-	}
-
-	public static void writeScore(String name, long score){
-		try{
-			fw = new FileWriter(file,true);
-			bw = new BufferedWriter(fw);
-			bw.write(name);
-			bw.newLine();
-			bw.write("" + score);
-			bw.newLine();
-			bw.close();
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	public void start(){
-		Thread thread = new Thread(this);
-		thread.start();//jump to run() below
-	}
-
-	public void stop(){
-		running = false;
-	}
-
+	
+	//main
 	public static void main(String args[]){
 		Game game = new Game();
 		j = new JFrame();
@@ -113,13 +91,21 @@ public class Game extends Canvas implements Runnable{
 		game.start();
 	}
 
+	public void start(){
+		Thread thread = new Thread(this);
+		thread.start();//jump to run() below
+	}
 
+	public void stop(){
+		running = false;
+	}
 
 	public void update(){
 		level.update();//calls click() inside of Level class to check for new clicks
 	}
 
 	public void render(){
+		//Set display for time counter
 		if(gameStart>0){
 			String time = new String("Time: " + (int)((currentTime-startTime)/100)/10 + "." + (int)((currentTime-startTime)/100)%10);
 			timeCount.setText(time);
@@ -127,37 +113,42 @@ public class Game extends Canvas implements Runnable{
 		else{
 			timeCount.setText("Time: 0.0");
 		}
+		
+		//Render the frame
 		Graphics g = image.getGraphics();
 		level.render(g);
 		g.drawRect(0, 0, 200, 200); //draws our 10x10 grid
 		g = getGraphics();
 		g.drawImage(image, 0, 0, width, height, 0, 0, width, height, null);//generates the outer window
 		g.dispose();
-		
 		bombCount.setText("Bombs:" + Level.gameCounter);
 	}
 
 
-
-	public void run(){//when thread.start() is called, it automatically jumps to this function
+	//when thread.start() is called, it automatically jumps to this function
+	public void run(){
 		gameStart=0;
 		new Tile();
-		windowrunning=true;
+		windowrunning = true;
 		running = true;
 		level = new Level();
 		
 		image = createVolatileImage(width,height);
 
+		//Run the application until the user tells it to close
 		while(windowrunning){
 			if(gameStart==1){
 				startTime = System.currentTimeMillis();
 				gameStart++;
 			}
+			
 			if(running)
 				currentTime = System.currentTimeMillis();
+			
 			update();//go to update()
 			render();//renders the grid after checking conditions and changing tile images
 
+			//If you hit a bomb, end the game
 			if(hitBomb){
 				Level.revealBombs(Level.grid);
 				update();
@@ -165,6 +156,8 @@ public class Game extends Canvas implements Runnable{
 				Level.endGame(false);
 				hitBomb=false;
 			}
+			
+			//If you win, end the game
 			if(gamewon){
 				update();
 				render();
@@ -172,17 +165,23 @@ public class Game extends Canvas implements Runnable{
 				Level.endGame(true);
 				gamewon=false;
 			}
+			
+			//Close the window
 			if(gtfo){
 				windowrunning=false;
 				j.setVisible(false);
 				j.dispose();
 			}
+			
+			//Wait a bit so as not to render more than necessary
 			try{
 				Thread.sleep(15);
 			} catch(Exception e){
 				e.printStackTrace();
 			}
 		}
+		
+		//Close the window when the application has been told to stop running
 		if(gtfo){
 			windowrunning=false;
 			j.setVisible(false);
@@ -190,7 +189,7 @@ public class Game extends Canvas implements Runnable{
 		}
 	}
 
-
+	//Reset the game board and current time/numbombs
 	public static void resetGame(){	
 		level=new Level();
 		Grid.numRevealed = 0;
@@ -199,6 +198,29 @@ public class Game extends Canvas implements Runnable{
 		ClickListener.useMouse=true;
 		running = true;
 		gameStart=0;
+	}
+
+	//Write high scores after game has been won
+	public static void writeScore(String name, long score){
+		try{
+			fw = new FileWriter(file,true);
+			bw = new BufferedWriter(fw);
+			bw.write(name);
+			bw.newLine();
+			bw.write("" + score);
+			bw.newLine();
+			bw.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	//Mouse Event checkers
+	public static boolean MouseLeft(){//checks for left click
+		return ClickListener.MouseLeft();
+	}
+	public static boolean MouseRight(){//checks for right click
+		return ClickListener.MouseRight();
 	}
 
 }
